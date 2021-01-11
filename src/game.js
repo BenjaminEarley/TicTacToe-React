009d940a4f1f242a8af069da2cae918e02297af7
Benjamin Earley <thebigben92@gmail.com>
2021-01-11 15:40:04 -0500

    writing custom hook and wrapping Game in new Home div

---
+++ src/game.js
@@ -0,0 +1,139 @@
+import React, {useState} from 'react';
+import './game.css';
+
+export function Game() {
+
+    const [squares, winner, xIsNext, history, handleMove, jumpTo] = useGameState()
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={squares}
+                    onClick={i => handleMove(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+function Board(props) {
+    function renderSquare(i) {
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => props.onClick(i)}
+            />
+        );
+    }
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+// ========================================
+
+function useGameState() {
+    const [history, setHistory] = useState([
+        {
+            squares: Array(9).fill(null)
+        }
+    ]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(1);
+
+    function handleMove(i) {
+        const slicedHistory = history.slice(0, stepNumber + 1);
+        const current = slicedHistory[slicedHistory.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? "X" : "O";
+
+        setHistory(slicedHistory.concat([
+            {
+                squares: squares
+            }
+        ]))
+        setStepNumber(slicedHistory.length)
+        setXIsNext(!xIsNext)
+    }
+
+    function jumpTo(step) {
+        setStepNumber(step)
+        setXIsNext((step % 2) === 0)
+    }
+
+    const current = history[stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    return [current.squares, winner, xIsNext, history, handleMove, jumpTo]
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
